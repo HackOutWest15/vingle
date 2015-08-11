7ae4311808adb907a807e3e25471763566144e8b
Daniel Rönnkvist <daniel@ronnkvist.com>
2015-08-11 19:11:50 +0200

    Search function

--- www/js/app/send/send.controller.js
+++ www/js/app/send/send.controller.js
@@ -6,11 +6,24 @@
     $scope.user.friends = $scope.user.friends.map(function(friend){
       friend.choosen = false;
       return friend;
-    })
+    });
+    $scope.friends = $scope.user.friends
+
 
     $scope.chooseFriend = function(friend){
       console.log("Clicked on " + friend.name);
       friend.choosen = !friend.choosen;
     }
+
+    $scope.$watch('searchQuery', function(newValue, oldValue){
+      if(newValue){
+        var regex = new RegExp(".*"+newValue.toLowerCase()+".*","g");
+        $scope.friends = $scope.user.friends.filter(function(f){
+          return f.name.toLowerCase().match(regex);
+        });
+      }else{
+        $scope.friends = $scope.user.friends;
+      }
+    });
   });
 }(angular.module('spotchat')))
