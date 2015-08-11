205d45aaa8de91bb1b717a7a19160cddeb3a3cdb
Cornelia Damber Håkansson <cornelia.damber@gmail.com>
2015-08-11 17:10:59 +0200

    Search function

--- www/js/app/draw/draw.controller.js
+++ www/js/app/draw/draw.controller.js
@@ -1,5 +1,6 @@
 (function(app){
-  app.controller('drawController', function($scope){
+  app.controller('drawController', function($scope, $rootScope){
     $scope.hej = "tja";
+    console.log($rootScope.track); 
   });
 }(angular.module('spotchat')))
--- www/js/app/search/search.controller.js
+++ www/js/app/search/search.controller.js
@@ -1,5 +1,23 @@
 (function(app){
-  app.controller('searchController', function($scope){
+  app.controller('searchController', function($scope, Spotify, $rootScope, $state){
     // start
+
+   $scope.searchTrack = function(searchQuery) {
+   		Spotify.search(searchQuery, 'track').then(function (data) {
+  			$scope.tracks = data.tracks.items; 
+
+  			console.log(data);
+
+		});
+   }
+
+   $scope.chooseTrack = function(track) {
+		$rootScope.track = track;
+		$state.go('draw')
+   		console.log(track); 
+   }
+
+   
+
   });
 }(angular.module('spotchat')))
