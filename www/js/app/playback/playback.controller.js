7cbc7e84f381dbde48fbb21be919277318a2fa0d
Daniel Rönnkvist <daniel@ronnkvist.com>
2015-08-12 14:13:07 +0200

    Playback information

--- www/js/app/playback/playback.controller.js
+++ www/js/app/playback/playback.controller.js
@@ -5,6 +5,15 @@
         $scope.track = $rootScope.playback.track;
         $scope.track.preview_urlTrusted = $sce.trustAsResourceUrl($scope.track.preview_url);
 
+        var audio = document.getElementById('audioPlayback');
+        $scope.timeLeft = 30;
+        setInterval(function(){
+          $scope.$apply(function(){
+            console.log(audio.duration - audio.currentTime);
+            $scope.timeLeft = Math.floor(audio.duration - audio.currentTime);
+          });
+        }, 1000)
+
         $scope.goBack = function(){
           $state.go($rootScope.previousState);
         }
