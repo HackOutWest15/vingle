638c7f0be5391e612e1dc9891e34cad08dd06a1b
Aleksandar Faraj <gusfaraal@student.gu.se>
2015-08-12 13:18:17 +0200

    bugfix search

--- www/js/app/search/search.controller.js
+++ www/js/app/search/search.controller.js
@@ -1,22 +1,23 @@
-(function(app){
-  app.controller('searchController', function($scope, Spotify, $rootScope, $state){
-    // start
+(function (app) {
+    app.controller('searchController', function ($scope, Spotify, $rootScope, $state) {
+        // start
+        var recentTimer;
+        $scope.$watch('searchInput', function (searchQuery) {
+            if (searchQuery) {
+                if (recentTimer) clearInterval(recentTimer);
+                recentTimer = window.setTimeout(function () {
+                    Spotify.search(searchQuery, 'track').then(function (data) {
+                        $scope.tracks = data.tracks.items;
+                        console.log(data);
+                    });
+                }, 500)
+            }
+        })
 
-    $scope.$watch('searchInput', function(searchQuery){
-      if(searchQuery){
-        window.setTimeout(function(){
-          Spotify.search(searchQuery, 'track').then(function (data) {
-            $scope.tracks = data.tracks.items;
-            console.log(data);
-          });
-        }, 1000)
-      }
-    })
-
-    $scope.chooseTrack = function(track) {
-      $rootScope.track = track;
-  		$state.go('draw');
-   		console.log(track);
-    }
-  });
+        $scope.chooseTrack = function (track) {
+            $rootScope.track = track;
+            $state.go('draw');
+            console.log(track);
+        }
+    });
 }(angular.module('spotchat')))
