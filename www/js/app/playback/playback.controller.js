bd2f92c02430d0e500a2d09efc281de4be7827c7
Aleksandar Faraj <gusfaraal@student.gu.se>
2015-08-12 12:39:42 +0200

    Added inbox

--- www/js/app/playback/playback.controller.js
+++ www/js/app/playback/playback.controller.js
@@ -1,7 +1,8 @@
 (function(app){
-    app.controller('playbackController', function($scope,$window,$rootScope){
+    app.controller('playbackController', function($scope,$sce,$window,$rootScope){
         $scope.lines = $rootScope.lines;
         console.log($rootScope.playback.track);
         $scope.track = $rootScope.playback.track;
+        $scope.track.preview_urlTrusted = $sce.trustAsResourceUrl($scope.track.preview_url);
     });
 }(angular.module('spotchat')));
